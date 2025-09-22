03efabc416d81c46af2a2fb0b23d071d2e91f90c
Dheekshitha <ramanagonidheekshithaa@gmail.com>
2025-09-22 13:56:29 +0530

    changes

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var a = 67;
 var a = 29;
 let b = 71;
-console.log(`value b is $(b)`);
+const c = 31
+
+console.log(`value b is ${b}`);
+console.log(`value b is ${c}`);
 console.log(a);
+// commit is used to understand the code by other developers
---
+++ datatype.js
@@ -0,0 +1,15 @@
+a = 71;
+console.log(typeof a);
+username = "fav";
+console.log(typeof username);
+c=true
+console.log(typeof c);
+data =null
+console.log(typeof data);
+d=undefined
+console.log(typeof d);
+
+array=[71,"dheekshu",true,null];
+console.log(typeof array)
+
+
